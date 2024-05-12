95a1c98ac635359c706b228eefb015945dd7b524
Noah Labrecque <noah.labrecque@gmail.com>
2024-05-12 16:38:09 +0000

    better asset build-process

--- assets/build-font.sh
+++
@@ -1,9 +0,0 @@
-msdf-atlas-gen \
-    -font ./custom-msdf.otf\
-    -type mtsdf \
-    -format png \
-    -size 64 \
-    -pxrange 4 \
-    -imageout ./custom.png \
-    -json ./custom-msdf.json \
-    -yorigin top \
