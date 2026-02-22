bb621ace344c3cff526918d30331d5c97c0166a6
Noah Labrecque <noah.labrecque@gmail.com>
2026-02-22 13:18:16 -0500

    cleanup comments

--- src/render/img_texture.rs
+++ src/render/img_texture.rs
@@ -87,25 +87,13 @@ impl ImageTexture {
         let texture_size = texture_size_of_image(&diffuse_img);
 
         let texture = device.create_texture(&wgpu::TextureDescriptor {
-            // All textures are stored as 3D, we represent our 2D texture
-            // by setting depth to 1.
             size: texture_size,
             mip_level_count: 1,
             sample_count: 1,
             dimension: wgpu::TextureDimension::D2,
-            // Most images are stored using sRGB, so we need to reflect that here.
             format: wgpu::TextureFormat::Rgba8Unorm,
-            // TEXTURE_BINDING tells wgpu that we want to use this texture in shaders
-            // COPY_DST means that we want to copy data to this texture
             usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
             label: Some("diffuse_texture"),
-            // This is the same as with the SurfaceConfig. It
-            // specifies what texture formats can be used to
-            // create TextureViews for this texture. The base
-            // texture format (Rgba8UnormSrgb in this case) is
-            // always supported. Note that using a different
-            // texture format is not supported on the WebGL2
-            // backend.
             view_formats: &[],
         });
 
